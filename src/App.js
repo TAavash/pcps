d5532394b0cd1f94facb6b567d283640ba1a8b12
Ramudgar <ramudgary52@gmail.com>
2024-07-10 07:42:32 +0545

    navbar is completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import "./App.css";
 import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Counter from "./counter";
+import CounterFunctional from "./counter_functional";
+import Index from "./components";
+// import GreetComponent from "./components/props/greetComponent";
+import { GreetRamComponent } from "./components/props/GreetRamComponent";
+import { GreetRuchiComponent } from "./components/props/greetRuchiComponent";
+import Navbar from "./components/Navbar/navbar";
 
 class App extends Component {
   render() {
     return (
-       <Counter/> 
+      // <>   
+      // <Navbar/>
+      //   <Counter/>
+      //  <CounterFunctional/> 
+      //  <Index/>
+      //  <GreetRamComponent/>
+      //  <GreetRuchiComponent/> 
+      //  </>
+
+      <Router>
+      <div>
+        <Navbar/>
+       <Routes>
+        <Route exact path="/" element={<Index />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/counter_functional" element={<CounterFunctional />} />
+        <Route path="/greet_ram" element={<GreetRamComponent />} />
+        <Route path="/greet_ruchi" element={<GreetRuchiComponent />} />
+       </Routes>
+ 
+      </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './tailwind.css';
 import Apps from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
